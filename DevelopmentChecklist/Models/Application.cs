916f8bba11bb5c2ddd60873c5bc07faeec8ce5f2
Jacob <jacobkepford@gmail.com>
2020-11-10 08:16:57 -0600

    Added Primary Key assignment to models and updated DB Connection string

--- DevelopmentChecklist/Models/Application.cs
+++ DevelopmentChecklist/Models/Application.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,6 +14,7 @@ namespace DevelopmentChecklist.Models
     
     public class Application
     {
+        [Key]
         public int AppID { get; set; }
         public string AppName { get; set; }
         public string AppURL { get; set; }
--- DevelopmentChecklist/Models/Note.cs
+++ DevelopmentChecklist/Models/Note.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace DevelopmentChecklist.Models
 {
     public class Note
     {
+        [Key]
         public int NoteID { get; set; }
         public int AppID { get; set; }
         public string NoteText { get; set; }
