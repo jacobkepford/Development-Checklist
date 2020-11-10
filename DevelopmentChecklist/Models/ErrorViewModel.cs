015176c6de76cda8a2f9815a636686b04c99bf30
Jacob <jacobkepford@gmail.com>
2020-11-09 18:47:21 -0600

    Initial Project Commit

---
+++ DevelopmentChecklist/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DevelopmentChecklist.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
